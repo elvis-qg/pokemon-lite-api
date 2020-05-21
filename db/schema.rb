ed7dd1df674b31ea39f4e308ed86dd9bdeb48dec
Andrés Del Carpio <andresdc@andresdc.net>
2020-05-21 18:36:21 -0500

    Remove age from Trainers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_21_212629) do
+ActiveRecord::Schema.define(version: 2020_05_21_233536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 2020_05_21_212629) do
   create_table "trainers", force: :cascade do |t|
     t.string "name"
     t.string "gender"
-    t.integer "age"
     t.string "home_region"
     t.boolean "status_active"
     t.integer "wins"
