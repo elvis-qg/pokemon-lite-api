e306bdf967ab0e05344c33324d8e2792b0c20002
Andrés Del Carpio <andresdc@andresdc.net>
2020-05-21 18:49:07 -0500

    Added validations to pokemon and trainer

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,4 +1,8 @@
 class Pokemon < ApplicationRecord
+  validates :name, :main_ability, :main_type, presence: true
+  validates :main_type, inclusion: { in: %W(bug electric fairy fighting fire flying grass ground ice normal poison psychic rock steel water) }
+  validates :name, uniqueness: true
+
   has_many :teams
   has_many :trainers, through: :teams
 end
