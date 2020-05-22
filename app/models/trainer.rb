3a1e48af1f9d2428215a94bc5c3ac70c21bf575d
Andrés Del Carpio <andresdc@andresdc.net>
2020-05-22 14:55:54 -0500

    Added more conditions to models

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -4,6 +4,6 @@ class Trainer < ApplicationRecord
   validates :name, uniqueness: true
   validates :wins, :losses, numericality: { greater_than_or_equal_to: 0 }
 
-  has_many :teams
+  has_many :teams, dependent: :destroy
   has_many :pokemons, through: :teams
 end
