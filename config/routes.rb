2b5ece9e3a86fb1d341377d3636d36ab7165af35
Andrés Del Carpio <andresdc@andresdc.net>
2020-05-22 14:56:04 -0500

    Added nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :teams
-  resources :trainers
-  resources :pokemons
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :trainers do
+    resources :pokemons, only: [:index, :show]
+  end
+
+  resources :pokemons  do
+    resources :trainers, only: [:index, :show]
+  end
 end
